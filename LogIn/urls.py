9fc27ee4c24a26e18340cb88fc9d71cd0504761c
Luis <lugearma@gmail.com>
2015-03-30 02:09:16 -0600

    Listo el Hola mundo

--- LogIn/urls.py
+++ LogIn/urls.py
@@ -6,9 +6,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'LogIn.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^hola/', views.holaMundo),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^/', views.home, name='home'),
-    url(r'^login/(\w*)', views.login, name='login'),
-    url(r'^simpleApp/' include(SimpleApp)),
+    #url(r'^/', views.home, name='home'),
+    #url(r'^login/(\w*)', views.login, name='login'),
+    #url(r'^simpleApp/' include(SimpleApp)),
+
 )
